164c20393f415b5897d99281378d6edc30adcb85
Amz K <akintner@u.rochester.edu>
2017-01-31 13:04:28 -0700

    add files to gitignore

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :reddit, 'bctKjkrzmWDBcA', '_2t3QusTKXWxjo9y8MMhsZW1Mks', 
+  provider :reddit, "#{ENV['REDDIT_CLIENT_ID']}", "#{ENV['REDDIT_CLIENT_SECRET']}", 
   scope: 'identity'
 end
