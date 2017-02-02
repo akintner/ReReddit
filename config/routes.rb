a54c18eb1fb748a6bc0039f646fbf2e2a95e9572
Amz K <akintner@u.rochester.edu>
2017-02-02 10:23:15 -0700

    update routes for pulling in post data on subreddits

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'rereddit#index'
   get '/auth/reddit', as: :reddit_login
   get '/auth/reddit/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   get 'dashboard', to: 'dashboard#index'
+  get '/r/subreddit_name/comments/:id/:post_name', to: "posts#show"
 end
